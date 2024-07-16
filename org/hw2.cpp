a1f15ad9b08bfb0535860e039e28528a1056b96d
birkenkrahe <birkenkrahe@lyon.edu>
2024-07-15 22:57:54 -0500

    Update from dell

---
+++ org/hw2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+void hello() { // function definition
+  cout << "hello" << endl;
+}
+main() { // program entry point
+  hello();  // function call
+}
